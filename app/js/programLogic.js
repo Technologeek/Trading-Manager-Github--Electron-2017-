d90342f0b4b3dcadfe404f7871116cac393bc144
Rahul Technologeek <rahulrules911@yahoo.com>
2017-01-14 21:38:03 +0530

    Added Cateory Modal and Minor UI changes

--- app/js/programLogic.js
+++ app/js/programLogic.js
@@ -1,19 +1,27 @@
-modalFunc();
 //Function to Load CRUD Page 
 function loadPage(){
 	window.document.location.href = 'Crud.html';
 }
-
 //Function to open modals and edit content
 function modalFunc() {
 	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 	   $('#modal1').modal();
 	   $('select').material_select();
-	}
-	$(function () {
-	 
+	   $(".second").hide();
+	   $(".first").show();
+	};
+//Function to open modal for Adding Catefory.
+/*I wil be opening the same modal but replacing the contents inside*/
+function modalFunc2(){
+	$('#modal1').modal();
+	$('select').material_select();
+	$( "div.first" ).toggleClass( "second" );
+	$(".second").show();
+	$(".first").hide();
+};	
+//Rating Stars Plugin initialization	
+	$(function () {	 
 	  $("#el").rateYo({
 	    rating: 3.6
-	  });
-	 
+	  });	 
 	});
