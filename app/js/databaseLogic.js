b0ecb582db264118922ea740dcfaedbd341c0b02
Rahul Technologeek <rahulrules911@yahoo.com>
2017-02-17 00:20:37 +0530

    DataBase Category Listing Done and code clean up

--- app/js/databaseLogic.js
+++ app/js/databaseLogic.js
@@ -1,67 +1,44 @@
+"use strict";
+//Declaring On-Page Load Functions
+$(document).ready(function() {
+selectList();
+});
 var Datastore = require('nedb');
-fs = require('fs');
-//*var clients = new Datastore({ filename: path.join(require('nw.gui').App.dataPath, 'clients.db') });
-
 var clients = new Datastore({ filename:  'clients.db', autoload: true });
 var category = new Datastore({ filename: 'category.db', autoload: true });
-var insertClient = function(name, categorys) {
+
+var insertClient = function(name,categorys){
 	var data = {
 		name: name,
 		categorys: categorys
-	};
-	clients.insert(data, function(err,doc){
-		console.log('Inserted', doc.name, 'with Id', doc._id, doc.categorys);
-	});
+	}
+	clients.insert(data,function(err,doc){
+	})
 };
 
-var insertCategory = (function(categs) {
+var insertCategory = function(categs) {
 	var categoryList = {
 		categs : categs
-	};
-	category.insert(categoryList, function(err,doc){
-		console.log('Inserted Category',doc.categs);
+	}
+	category.insert(categoryList,function(err,doc){
+	})
+};
+
+var selectList = function(categs){
+		category.find({},function(err,doc){
+			doc.forEach(function(catlists){
+				console.log(cata)
+				$('#category_select').append('<option>'+catlists.categs+'</option>');
+		})
+	})
+
+};
+
+/*//Function Deletes all the categories from the database.Uncomment only when needed.
+(function(){
+	category.remove({}, { multi: true }, function (err, numRemoved) {
+		console.log("removed!");
 	});
-	console.log(categs);
-	category.find({},function(err,doc){
-		console.log(categs);
-		doc.forEach(function(categoryList){
-			console.log(doc.categoryList)
-			$('#category_select').append($('<option>',{value : categoryList.value},'</option>'));
-	});
-	/*$.each(categoryList, function(key, value){
-		$('#category_name').append($('<option>', + {categs : key}).text(categs));
-	})*/
-});
+
 })();
-
- var searchClients = function(name){
-      /*var data2 = {
-      name : name 
-      };*/	
-      clients.find({name: 'Rahul'}, function(err,doc) {
-      console.log('Found the name',doc);
-      });
- };
-/*var data = {
-	name : 'tupac',
-	categorys : 'paypal'
-
-}; 
-
-clients.insert(data, function(err,doc){
-	console.log('Inserted', doc.name, 'with Id', doc._id, doc.categorys);
-});*/
-
-/*category.inert(data function (err,doc){
-console.log('Inserted', doc.categorys)
-});*/
-
-/*clients.findOne({ categorys: 'paypal' }, function(err, doc) {  
-    console.log('Found user:', doc.name);
-});*/
-
-
-// Create a "unique" index for the clients name and categories
-/*clients.ensureIndex({fieldName: 'name', unique: true});
-clients.ensureIndex({fieldName: 'category', unique: true});
 */
